1245da2f6647a3eceea7a44377f71c5b56f1149e
Vinayak Gupta <vinayakgupta3112@gmail.com>
2025-10-16 21:08:53 +0530

    Updated models

--- src/main/java/com/example/uberprojectentityservice/models/Booking.java
+++ src/main/java/com/example/uberprojectentityservice/models/Booking.java
@@ -11,6 +11,9 @@ import java.util.Date;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+//@Table(indexes = {
+//        @Index(columnList = "driver_id")
+//})
 public class Booking extends BaseModel{
 
     @Enumerated(value = EnumType.STRING)
@@ -29,5 +32,11 @@ public class Booking extends BaseModel{
 
     @ManyToOne(fetch = FetchType.LAZY)
     private Passenger passenger;
+
+    @OneToOne
+    private ExactLocation startLocation;
+
+    @OneToOne
+    private ExactLocation endLocation;
 
 }
